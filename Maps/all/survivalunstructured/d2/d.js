81ed8b1609035aa7d05556bcb2ce400eb13748b3
NonKun Ch <92234807+NonCraftStudio@users.noreply.github.com>
2024-06-30 16:46:10 +0700

    Update d.js

--- Maps/all/survivalunstructured/d2/d.js
+++ Maps/all/survivalunstructured/d2/d.js
@@ -1,7 +1,7 @@
 function downloadFile() {
     const link = document.createElement('a');
     link.href = 'f/Survival_Unstructured.zip'; // ใส่ URL ของไฟล์ที่ต้องการดาวน์โหลด
-    link.download = 'your_file.zip'; // ชื่อไฟล์ที่จะถูกดาวน์โหลด
+    link.download = 'Survival_Unstructured'; // ชื่อไฟล์ที่จะถูกดาวน์โหลด
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
