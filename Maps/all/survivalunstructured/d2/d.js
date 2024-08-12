bd8a1ac29c9fca86dde4cea93c8e548b50e77c22
NonKun Ch <92234807+NonCraftStudio@users.noreply.github.com>
2024-08-12 11:01:39 +0700

    Delete Maps directory

--- Maps/all/survivalunstructured/d2/d.js
+++
@@ -1,8 +0,0 @@
-function downloadFile() {
-    const link = document.createElement('a');
-    link.href = 'f/Survival_Unstructured.zip'; // ใส่ URL ของไฟล์ที่ต้องการดาวน์โหลด
-    link.download = 'Survival_Unstructured'; // ชื่อไฟล์ที่จะถูกดาวน์โหลด
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
